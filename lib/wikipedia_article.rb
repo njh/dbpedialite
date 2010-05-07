da943109aa5883cc35c35de4eb779ac566db1d54
Nicholas Humfrey <humfrn01@MC-S001860-2.local>
2010-05-07 12:00:39 +0100

    Re-factored the wikipedia API code to put the HTTP code in a new method

--- lib/wikipedia_article.rb
+++ lib/wikipedia_article.rb
@@ -6,14 +6,25 @@ require 'uri'
 
 class WikipediaArticle < OpenStruct
   USER_AGENT = 'DbpediaLite/1'
+  WIKIPEDIA_API = URI.parse('http://en.wikipedia.org/w/api.php')
 
   def self.find(args)
-    uri_str = "http://en.wikipedia.org/w/api.php?action=query&redirects&prop=info&format=json"
-    args.each_pair do |key,value|
-      uri_str += '&'+CGI::escape(key.to_s)+'='+CGI::escape(value.to_s)
-    end
+  
+    data = api_get({:action => 'query', :redirects => 1, :prop => 'info'}.merge(args))
+    puts data
     
-    uri = URI.parse(uri_str)
+    # FIXME: check that a single page has been returned
+    data['query']['pages'].values.first
+  end
+  
+  def self.api_get(args)
+    items = ['format=json']
+    args.each_pair do |key,value|
+     items.unshift CGI::escape(key.to_s)+'='+CGI::escape(value.to_s)
+    end
+
+    uri = WIKIPEDIA_API.clone
+    uri.query = items.join('&')
     res = Net::HTTP.start(uri.host, uri.port) do |http|
       http.get(uri.request_uri, {'User-Agent' => USER_AGENT})
     end
@@ -21,9 +32,7 @@ class WikipediaArticle < OpenStruct
     # Throw exception if unsuccessful
     res.value
     
-    # FIXME: check that a single page has been returned
-    
-    self.new(JSON.parse(res.body)['query']['pages'].values.first)
+    JSON.parse(res.body)
   end
   
   def initialize(args=nil)
