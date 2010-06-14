ee13e76c84cbe47c87d8a33e0c4f39f1c2940ec4
Nicholas J Humfrey <njh@aelius.com>
2010-06-14 09:55:29 +0100

    Implemented some of the Freebase API

---
+++ lib/freebase_api.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'net/http'
+require 'json'
+require 'uri'
+require 'cgi'
+
+#http://rdf.freebase.com/rdf/m.03rf2x
+
+module FreebaseApi
+  USER_AGENT = 'DbpediaLite/1'
+  MQLREAD_URI = URI.parse('http://www.freebase.com/api/service/mqlread')
+  RDF_BASE_URI = URI.parse('http://rdf.freebase.com/rdf/')
+
+  def self.mqlread(query)
+    uri = MQLREAD_URI.clone
+    uri.query = 'query='+CGI::escape(JSON.dump({'query' => query}))
+    res = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.get(uri.request_uri, {'User-Agent' => USER_AGENT})
+    end
+
+    # Throw an exception if HTTP request was unsuccessful
+    res.value
+
+    # Throw and exception if the JSON response was unsuccessful
+    data = JSON.parse(res.body)
+    unless data['code'] == '/api/status/ok'
+      raise "Freebase query failed: #{data['messages'][0]['message']}" 
+    end
+    
+    data['result']
+  end
+
+  def self.lookup_wikipedia_pageid(pageid, language='en')
+    mqlread({
+      'key' => {
+        'namespace' => "/wikipedia/#{language}_id",
+        'value' => pageid.to_s,
+        'limit' => 0
+       },
+       'id' => nil,
+       'name' => nil,
+       'mid' => nil,
+       'guid' => nil,
+       'limit' => 1
+    })
+  end  
+end
---
+++ spec/freebase_api.spec
@@ -0,0 +1,39 @@
+require File.dirname(__FILE__) + "/spec_helper.rb"
+require 'freebase_api'
+
+describe FreebaseApi do
+
+  context "looking up the Freebase MID for a Wikipedia Pageid" do
+    before :each do
+      response = mock(
+        :value => nil,
+        :body => fixture_data('freebase-mqlread-ceres.json')
+      )
+      Net::HTTP.expects(:start).once.returns(response)
+      @data = FreebaseApi.lookup_wikipedia_pageid(934787)
+    end
+
+    it "should not return nil" do
+      @data.should_not be_nil
+    end
+    
+    it "should return the name of the topic" do
+      @data['name'].should == 'Ceres'
+    end
+    
+    it "should return the GUID for the topic" do
+      @data['guid'].should == '#9202a8c04000641f80000000003bb45c'
+    end
+    
+    it "should return the Machine ID for the topic" do
+      @data['mid'].should == '/m/03rf2x'
+    end
+    
+    it "should return the Freebase ID for the topic" do
+      @data['id'].should == '/en/ceres_united_kingdom'
+    end
+    
+    
+  end
+
+end
